1d889b4355f92bb2206aa8a28dc16051e29a915d
Mohammed Arif <89202030+mohammed-mm1908981@users.noreply.github.com>
2023-06-18 14:28:05 +0300

    Added material table and api fetch

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'e-comm'`, () => {
+  it(`should have as title 'hospital-system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('e-comm');
+    expect(app.title).toEqual('hospital-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('e-comm app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hospital-system app is running!');
   });
 });
